8a972a7c84ca3d3e5f9b4f1dcc1db24c4d4277f5
Stephan Hagemann <stephan.hagemann@gmail.com>
2024-02-12 23:05:47 -0700

    Remove circular dependency from rails_shims

--- packs/posts/app/public/posts/config.rb
+++ packs/posts/app/public/posts/config.rb
@@ -1,3 +1,8 @@
 class Posts::Config
   ROUTE_FILE_NAME = "posts"
+
+  # Entrypoint helper for posts
+  def self.entrypoint_path(book_name = nil)
+    Rails.application.routes.url_helpers.posts_path
+  end
 end
--- packs/speaking/app/public/speaking/config.rb
+++ packs/speaking/app/public/speaking/config.rb
@@ -1,3 +1,8 @@
 class Speaking::Config
   ROUTE_FILE_NAME = "speaking"
+
+  # Entrypoint helper for speaking
+  def self.entrypoint_path(book_name = nil)
+    Rails.application.routes.url_helpers.speaking_index_path
+  end
 end
