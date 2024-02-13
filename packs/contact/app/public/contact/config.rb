52776f33f731db9a15fba0b943fad7809f1e3a0a
Stephan Hagemann <stephan.hagemann@gmail.com>
2024-02-12 19:38:11 -0700

    Expose path helper for contact page via public API

--- packs/contact/app/public/contact/config.rb
+++ packs/contact/app/public/contact/config.rb
@@ -1,3 +1,8 @@
 class Contact::Config
   ROUTE_FILE_NAME = "contact"
+
+  # Exposes path helper to root of contact page. You can pass in a subject as a param
+  def self.entrypoint_path(subject)
+    Rails.application.routes.url_helpers.contact_index_path(subject: subject)
+  end
 end
