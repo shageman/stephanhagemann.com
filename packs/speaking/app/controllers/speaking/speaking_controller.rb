ecd2f8f50868760f3a889564a7b1ff9424f4b0c0
Stephan Hagemann <stephan.hagemann@gmail.com>
2024-02-12 19:11:18 -0700

    Render speaking on page layout

--- packs/speaking/app/controllers/speaking/speaking_controller.rb
+++ packs/speaking/app/controllers/speaking/speaking_controller.rb
@@ -4,6 +4,8 @@ class Speaking::SpeakingController < ApplicationController
   def index
     @upcoming_talks = Speaking::Talk.all.select { Date.parse(_1.date) > Time.current }.sort_by { Date.parse(_1.date) }
     @past_talks = Speaking::Talk.all.select { Date.parse(_1.date) <= Time.current }.sort_by { Date.parse(_1.date) }.reverse
+    
+    render layout: "page"
   end
 
   private
