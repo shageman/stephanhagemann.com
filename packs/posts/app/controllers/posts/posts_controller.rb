432ee48482496093131083b28850f9558540002a
Stephan Hagemann <stephan.hagemann@gmail.com>
2024-02-26 06:26:26 -0700

    Show webmentions info on all post pages

--- packs/posts/app/controllers/posts/posts_controller.rb
+++ packs/posts/app/controllers/posts/posts_controller.rb
@@ -6,20 +6,21 @@ class Posts::PostsController < ApplicationController
 
   def show
     @post = Posts::Post.find_by_slug(params[:id]) || Posts::MastodonPost.find_by_slug(params[:id])
-    if @post.is_a?(Posts::Post)
-      @likes =
-        Posts::Webmention.where(:"wm-target" => "https://stephanhagemann.com/posts/#{@post.slug}/", :"wm-property" => "like-of").to_a +
-        Posts::Webmention.where(:"wm-target" => "https://stephanhagemann.com/posts/#{@post.slug}", :"wm-property" => "like-of").to_a
 
-      @reposts =
-        Posts::Webmention.where(:"wm-target" => "https://stephanhagemann.com/posts/#{@post.slug}/", :"wm-property" => "repost-of").to_a +
-        Posts::Webmention.where(:"wm-target" => "https://stephanhagemann.com/posts/#{@post.slug}", :"wm-property" => "repost-of").to_a
+    @likes =
+    Posts::Webmention.where(:"wm-target" => "https://stephanhagemann.com/posts/#{@post.slug}/", :"wm-property" => "like-of").to_a +
+    Posts::Webmention.where(:"wm-target" => "https://stephanhagemann.com/posts/#{@post.slug}", :"wm-property" => "like-of").to_a
 
-      replies =
-        Posts::Webmention.where(:"wm-target" => "https://stephanhagemann.com/posts/#{@post.slug}/", :"wm-property" => ["mention-of", "in-reply-to"]).to_a +
-        Posts::Webmention.where(:"wm-target" => "https://stephanhagemann.com/posts/#{@post.slug}", :"wm-property" => ["mention-of", "in-reply-to"]).to_a
-      @replies = replies.sort_by { Date.parse(_1[:published]) }.reverse
-    elsif @post.is_a?(Posts::MastodonPost)
+    @reposts =
+      Posts::Webmention.where(:"wm-target" => "https://stephanhagemann.com/posts/#{@post.slug}/", :"wm-property" => "repost-of").to_a +
+      Posts::Webmention.where(:"wm-target" => "https://stephanhagemann.com/posts/#{@post.slug}", :"wm-property" => "repost-of").to_a
+
+    replies =
+      Posts::Webmention.where(:"wm-target" => "https://stephanhagemann.com/posts/#{@post.slug}/", :"wm-property" => ["mention-of", "in-reply-to"]).to_a +
+      Posts::Webmention.where(:"wm-target" => "https://stephanhagemann.com/posts/#{@post.slug}", :"wm-property" => ["mention-of", "in-reply-to"]).to_a
+    @replies = replies.sort_by { Date.parse(_1[:published]) }.reverse
+
+    if @post.is_a?(Posts::MastodonPost)
       @related_posts = @post.context && @post.context["descendants"]
     end
   end
