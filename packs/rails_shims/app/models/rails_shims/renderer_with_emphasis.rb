5645b7b32b6bb44630d664c24d775408e57f110a
Stephan Hagemann <stephan.hagemann@gmail.com>
2024-02-26 15:34:00 -0700

    Markdown rendering: no need to customize. Activate extensions

--- packs/rails_shims/app/models/rails_shims/renderer_with_emphasis.rb
+++
@@ -1,5 +0,0 @@
-class RailsShims::RendererWithEmphasis < Redcarpet::Render::HTML
-  def preprocess(full_document)
-    full_document.gsub(/==([^=]+)==/, '<mark>\1</mark>')
-  end
-end
--- packs/rails_shims/app/public/application_controller.rb
+++ packs/rails_shims/app/public/application_controller.rb
@@ -5,7 +5,12 @@ class ApplicationController < ActionController::Base
   # Adds a markdown renderer instance variable, which allows variables to be rendered from a markdown source.
   # Use in view like so: `raw @markdown.render(@some_markdown_content)`
   def load_markdown_renderer
-    @markdown = Redcarpet::Markdown.new(RailsShims::RendererWithEmphasis, extensions = {})
+    @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, {
+      autolink: true, # naked links become HTML links
+      highlight: true, # == for <mark> highlighting
+      footnotes: true, # links to footnotes work with [^1]
+      lax_spacing: true, # don't require empty lines around HTML, just newlines
+    })
   end
 
   # Sets the default Layout param
