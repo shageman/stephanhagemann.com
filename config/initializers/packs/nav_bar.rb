b28d74e908e7e913cd901418dba9dd11096d0174
Stephan Hagemann <stephan.hagemann@gmail.com>
2024-02-26 22:15:08 -0700

    Routes and NavBar: resilient against many packs being deleted

--- config/initializers/packs/nav_bar.rb
+++ config/initializers/packs/nav_bar.rb
@@ -2,12 +2,12 @@
 Rails.application.config.to_prepare do
   Rails.application.reload_routes!
 
-  NavBar::Menu.add_item(section: "root", path:  Root::Api.entrypoint_path, text: "Stephan Hagemann", class_name: "home")
+  NavBar::Menu.add_item(section: "root", path: Root::Api.entrypoint_path, text: "Stephan Hagemann", class_name: "home") if defined?(Root::Api)
 
-  NavBar::Menu.add_item(section: "posts", path: Posts::Api.entrypoint_path, text: "Posts")
-  NavBar::Menu.add_item(section: "books", path: Books::Api.entrypoint_path, text: "Books")
-  NavBar::Menu.add_item(section: "speaking", path: Speaking::Api.entrypoint_path, text: "Speaking")
-  NavBar::Menu.add_item(section: "services", path: Services::Api.entrypoint_path, text: "Services")
-  NavBar::Menu.add_item(section: "tags", path: Tags::Api.entrypoint_path, text: "Tags")
-  NavBar::Menu.add_item(section: "agile_landscape", path: AgileLandscape::Api.entrypoint_path, text: "Agile Landscape")
+  NavBar::Menu.add_item(section: "posts", path: Posts::Api.entrypoint_path, text: "Posts") if defined?(Posts::Api)
+  NavBar::Menu.add_item(section: "books", path: Books::Api.entrypoint_path, text: "Books") if defined?(Books::Api)
+  NavBar::Menu.add_item(section: "speaking", path: Speaking::Api.entrypoint_path, text: "Speaking") if defined?(Speaking::Api)
+  NavBar::Menu.add_item(section: "services", path: Services::Api.entrypoint_path, text: "Services") if defined?(Services::Api)
+  NavBar::Menu.add_item(section: "tags", path: Tags::Api.entrypoint_path, text: "Tags") if defined?(Tags::Api)
+  NavBar::Menu.add_item(section: "agile_landscape", path: AgileLandscape::Api.entrypoint_path, text: "Agile Landscape") if defined?(AgileLandscape::Api)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,15 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
-  draw(Root::Api::ROUTE_FILE_NAME)
-  draw(NavBar::Api::ROUTE_FILE_NAME)
+  draw(NavBar::Api::ROUTE_FILE_NAME) if defined?(NavBar::Api)
+  draw(Contact::Api::ROUTE_FILE_NAME) if defined?(Contact::Api)
+  draw(Tags::Api::ROUTE_FILE_NAME) if defined?(Tags::Api)
 
-  draw(AgileLandscape::Api::ROUTE_FILE_NAME)
-  draw(Books::Api::ROUTE_FILE_NAME)
-  draw(Contact::Api::ROUTE_FILE_NAME)
-  draw(Posts::Api::ROUTE_FILE_NAME)
-  draw(Services::Api::ROUTE_FILE_NAME)
-  draw(Speaking::Api::ROUTE_FILE_NAME)
-  draw(Tags::Api::ROUTE_FILE_NAME)
+  draw(Root::Api::ROUTE_FILE_NAME) if defined?(Root::Api)
+
+  draw(AgileLandscape::Api::ROUTE_FILE_NAME) if defined?(AgileLandscape::Api)
+  draw(Books::Api::ROUTE_FILE_NAME) if defined?(Books::Api)
+  draw(Posts::Api::ROUTE_FILE_NAME) if defined?(Posts::Api)
+  draw(Services::Api::ROUTE_FILE_NAME) if defined?(Services::Api)
+  draw(Speaking::Api::ROUTE_FILE_NAME) if defined?(Speaking::Api)
 end
