36368986c56ebdae46cd8cd5356f667b90526180
Stephan Hagemann <stephan.hagemann@gmail.com>
2024-02-28 15:35:59 -0700

    Add spec for books API

--- packs/books/spec/books_spec.rb
+++ packs/books/spec/books_spec.rb
@@ -1,8 +1,14 @@
 # typed: false
 require "rails_helper"
 
-RSpec.describe Books do
-  it "knows the truth" do
-    expect(1).to eq 1
+RSpec.describe Books::Api do
+  describe "#entrypoint_path" do
+    it "without a param, it returns the index path" do
+      expect(Books::Api.entrypoint_path).to eq "/books"
+    end
+
+    it "with a param, it returns the show path" do
+      expect(Books::Api.entrypoint_path("some_book")).to eq "/books/some_book"
+    end
   end
 end
