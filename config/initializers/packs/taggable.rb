c08ce856c395d806c32720d0161cea483facd7af
Stephan Hagemann <stephan.hagemann@gmail.com>
2024-03-09 09:08:39 -0700

    Remove extraneous puts

--- config/initializers/packs/taggable.rb
+++ config/initializers/packs/taggable.rb
@@ -5,7 +5,6 @@ require "singleton"
 Rails.application.config.to_prepare do
   if defined?(Taggable)
     if defined?(Tags)
-      puts "blah"
       Taggable::TagServer.configure_tagger(Tags::Api)
     else
       Taggable::TagServer.configure_tagger
